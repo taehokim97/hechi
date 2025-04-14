74ca4450a8b9f08439b93bed5c159f38c192ca41
Taeho Kim <sinabero3271@gmail.com>
2025-04-14 15:28:35 +0900

    Create initial config and metadata files

---
+++ src/hechi/__about__.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.1"
+__version_binary__ = "1.0.0"
+__author__ = "Taeho Kim"
+__license__ = "MIT"
---
+++ src/hechi/__init__.py
@@ -0,0 +1,8 @@
+from .__about__ import __version__, __version_binary__, __author__, __license__
+
+all = (
+    "__author__",
+    "__license__",
+    "__version__",
+    "__version_binary__",
+)
